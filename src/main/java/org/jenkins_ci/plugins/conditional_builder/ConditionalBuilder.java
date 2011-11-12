a12b7b489b1c800c4abeb04ae3ed1e0c7610dfd7
bap2000 <bap-git@a1.org.uk>
2011-11-12 11:59:27 +0000

    Explicitly default condition and runner to Always and Fail .

--- src/main/java/org/jenkins_ci/plugins/conditional_builder/ConditionalBuilder.java
+++ src/main/java/org/jenkins_ci/plugins/conditional_builder/ConditionalBuilder.java
@@ -40,6 +40,7 @@ import hudson.tasks.Builder;
 import net.sf.json.JSONObject;
 import org.jenkins_ci.plugins.run_condition.BuildStepRunner;
 import org.jenkins_ci.plugins.run_condition.RunCondition;
+import org.jenkins_ci.plugins.run_condition.core.AlwaysRun;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.Stapler;
 import org.kohsuke.stapler.StaplerRequest;
@@ -145,8 +146,16 @@ public class ConditionalBuilder extends Builder {
             return BuildStepRunner.all();
         }
 
+        public BuildStepRunner.BuildStepRunnerDescriptor getDefaultBuildStepRunner() {
+            return Hudson.getInstance().getDescriptorByType(BuildStepRunner.Fail.FailDescriptor.class);
+        }
+
         public List<? extends Descriptor<? extends RunCondition>> getRunConditions() {
             return RunCondition.all();
+        }
+
+        public RunCondition.RunConditionDescriptor getDefaultRunCondition() {
+            return Hudson.getInstance().getDescriptorByType(AlwaysRun.AlwaysRunDescriptor.class);
         }
 
         public List<? extends Descriptor<? extends BuildStep>> getAllowedBuilders(AbstractProject<?,?> project) {
